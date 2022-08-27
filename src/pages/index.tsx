8b28a0a3e2b62653a5e67f8b0c7a180e8d9b60a9
ItIsLouis <louisbus7860@gmail.com>
2022-08-27 02:11:48 -0700

    man unt

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Non-Official Man United Site</title>
+        <meta name="description" content="The Non-Official Manchester United website with news, fixtures, videos, tickets, live match coverage, match highlights, player profiles, transfers, ..." />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      <main>
+
+      </main>
+    </div>
+  )
+}
+
+export default Home
